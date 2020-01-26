c418280618e95340e9e7f5d00800235bc4a8b2b1
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-01-26 18:38:14 +0800

    Create script.js

---
+++ Odd Up, Even Down/script.js
@@ -0,0 +1,9 @@
+function transform(arr) {
+  let result = [];
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] % 2 === 0) {arr[i] -= 1}
+    else {arr[i] += 1}
+    result.push(arr[i]);
+  }
+  return result;
+}
