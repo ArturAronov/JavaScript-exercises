7c6b1621bf49caf9c670f5b49497908667e540e9
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-03-05 19:33:15 +0800

    Create result.js

---
+++ Multiples of 3 or 5/result.js
@@ -0,0 +1,12 @@
+function test(n){
+    let arr = []
+    for (let i=1; i<n; i++) {
+        if (i%3 === 0) {arr.push(i)}
+        else if (i%5 === 0) {arr.push(i)}
+    }
+    let sum = arr.reduce(function (a,b) {
+        return a+b;
+    })
+    return sum;
+}
+test(100)
