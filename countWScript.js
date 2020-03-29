a91779b0242def8c65df5c3c53f1cdc6b40aa265
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-03-29 12:46:31 +0800

    Delete countWScript.js

--- countWScript.js
+++
@@ -1,9 +0,0 @@
-let txt = document.querySelector('#idText');
-let result = document.querySelector('#result');
-let resultC = document.querySelector('#result');
-
-txt.onkeydown = function() {
-    let num = txt.value.split(" ").length;
-    result.innerHTML = `You have entered ${num} words.`
-    return result;
-}
