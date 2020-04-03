35fecac82c7a3d8a48483710094f8bc2714a2bc4
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-04-03 16:09:05 +0800

    Create script.js

---
+++ Number of Steps to Reduce a Number to Zero/script.js
@@ -0,0 +1,14 @@
+var numberOfSteps  = function(num) {
+    let arr = [];
+    let i=0;
+    while (i<num) {
+        if (num % 2 === 0) {
+            num /=  2;
+            arr.push(num)
+        } else {
+            num -= 1;
+            arr.push(num);
+        }
+    }
+    return arr.length;
+};
