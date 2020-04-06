2dfb52b27443647fbd84e7c544e64eb293ae3393
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-04-06 14:56:45 +0800

    Update script.js

--- Bon Appétit/script.js
+++ Bon Appétit/script.js
@@ -1,11 +1,14 @@
 //Test case: [3,10,2,9], 1, 7
 //Result: Bon Apetit
 
+//Test case: [3,10,2,9], 1, 10
+//Result: 3
+
 function bonAppetit(bill, k, b) {
-    const calculator = (a,b) => a+b
+    const calculator = (a,b) => a+b;
     bill.splice(k,1);
     const sum = bill.reduce(calculator)/2;
-    const difference = b - sum
+    const difference = b - sum;
     if(difference === 0) {console.log("Bon Appetit")} 
     else {console.log(difference)}
 }
