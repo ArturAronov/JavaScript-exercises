0edd9d08def1af30385291b76a134df565dce9a5
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-04-16 01:04:29 +0800

    Create script.js

---
+++ Find Digits/script.js
@@ -0,0 +1,14 @@
+//Test case: 1012
+//Result: 3
+
+function findDigits(n) {
+    let count=0;
+    let d=n.toString().split('');
+    console.log(d)
+    for (let i=0; i<d.length; i++) {
+        if(n%d[i]===0) {
+            count++
+        }
+    }
+    return count;
+}
