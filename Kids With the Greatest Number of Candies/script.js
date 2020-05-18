4749e3b5ec6099f6fe87084f2bdd017090427cc9
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-05-18 12:39:10 +0800

    Create script.js

---
+++ Kids With the Greatest Number of Candies/script.js
@@ -0,0 +1,8 @@
+let kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies);
+    let result = [];
+    for (let i=0; i<candies.length; i++) {
+        result.push(candies[i]+extraCandies>=max);
+    };
+    return result;
+};
