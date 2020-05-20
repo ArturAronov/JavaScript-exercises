139d71b7d4b8cfe311ffe59844b413750a4b4b23
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-05-20 15:53:12 +0800

    Update factorial.js

--- Recursions/factorial.js
+++ Recursions/factorial.js
@@ -4,20 +4,20 @@
 //Recursion:
 function factorial(n) {
     if (n===0) {
-        return 1
+        return 1;
     } else {
         return n*factorial(n-1);
-    }
-}
+    };
+};
 
 
-//For lopp:
+//For-loop:
 function factorial(n) {
     let arr=[];
     let reducer = (a,b) => a*b;
     for (let i=1; i<=n; i++) {
         arr.push(i);
-    }
+    };
     let result = arr.reduce(reducer);
     return result;
-}
+};
