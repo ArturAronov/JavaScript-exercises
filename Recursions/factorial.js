63ecb6beb813fe1fde28b85924e60af938247b00
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-05-20 15:49:51 +0800

    Create factorial.js

---
+++ Recursions/factorial.js
@@ -0,0 +1,23 @@
+//Test-case: 10
+//Result: 3628800
+
+//Recursion:
+function factorial(n) {
+    if (n===0) {
+        return 1
+    } else {
+        return n*factorial(n-1);
+    }
+}
+
+
+//For lopp:
+function factorial(n) {
+    let arr=[];
+    let reducer = (a,b) => a*b;
+    for (let i=1; i<=n; i++) {
+        arr.push(i);
+    }
+    let result = arr.reduce(reducer);
+    return result;
+}
