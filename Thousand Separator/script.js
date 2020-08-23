0e3c8abe70888f70f5e824a9f101b1f58f0b27d4
Artur Aronov <52620016+ArturAronov@users.noreply.github.com>
2020-08-23 12:05:05 +0800

    Create script.js

---
+++ Thousand Separator/script.js
@@ -0,0 +1,7 @@
+var thousandSeparator = function(n) {
+  const arr=n.toString().split('').reverse();
+  return arr.map((x,index)=>{
+    if(index%3===0&&index!==0) x+='.'
+    return x;
+  }).reverse().join('');
+};
